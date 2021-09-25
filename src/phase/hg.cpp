ffa98196321ff0c9b94ee8f8721ab5675e911ed4
mbks <s6mabusk@uni-bonn.de>
2021-09-25 13:37:49 +0200

    changed hg slightly

--- src/phase/hg.cpp
+++ src/phase/hg.cpp
@@ -42,10 +42,7 @@ public:
 
     HGPhaseFunction(const Properties &props) : Base(props) {
         m_g = props.float_("g", 0.8f);
-/*
-        if (m_g >= 1 || m_g <= -1)
-            Log(Error, "The asymmetry parameter must lie in the interval (-1, 1)!");
-*/
+
         m_flags = +PhaseFunctionFlags::Anisotropic;
     }
 
@@ -59,13 +56,11 @@ public:
                                       Mask active) const override {
         MTS_MASKED_FUNCTION(ProfilerPhase::PhaseFunctionSample, active);
 
-	Mask my_mask = enoki::abs(m_g) < math::Epsilon<ScalarFloat>;
+        Float cos_theta;
+        Mask my_mask = enoki::abs(m_g) < math::Epsilon<ScalarFloat>;
+        masked(cos_theta, my_mask) = 1 - 2 * sample.x();
+        masked(cos_theta, !my_mask) = (1 + m_g * m_g - ((1 - m_g * m_g) / (1 - m_g + 2 * m_g * sample.x())) * ((1 - m_g * m_g) / (1 - m_g + 2 * m_g * sample.x()))) / (2 * m_g);
 
-	Float sqr_term = (1 - m_g * m_g) / (1 - m_g + 2 * m_g * sample.x());
-	Float cos_theta_2 = (1 + m_g * m_g - sqr_term * sqr_term) / (2 * m_g);
-	Float cos_theta_1 = 1 - 2 * sample.x();
-
-	Float cos_theta = enoki::select(my_mask, cos_theta_1, cos_theta_2);
         Float sin_theta = enoki::safe_sqrt(1.0f - cos_theta * cos_theta);
         auto [sin_phi, cos_phi] = enoki::sincos(2 * math::Pi<ScalarFloat> * sample.y());
         auto wo = Vector3f(sin_theta * cos_phi, sin_theta * sin_phi, cos_theta);
